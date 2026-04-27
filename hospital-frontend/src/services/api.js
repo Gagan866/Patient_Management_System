16dedb1e5837b7aa6d7e85b2182295b9466b6b61
Gagan866 <gd0363264@gmail.com>
2026-04-27 22:28:31 +0530

    MVP DONE

--- hospital-frontend/src/services/api.js
+++ hospital-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: "https://patient-management-system-1cc1.onrender.com",
 })
 
 api.interceptors.request.use((config) => {
